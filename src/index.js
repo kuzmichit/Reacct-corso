66dae482ed6a2d8274ea1c19754cdbe45308a97e
kuzmichit <kuzmichit@gmail.com>
2022-10-22 11:15:48 +0200

    finito la pagina del progetto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './components/app/app.js';
 
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = createRoot(
+  document.getElementById('root'),
+);
+
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
