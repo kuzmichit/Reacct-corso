248407093fdc372713cc710afd24104605f7bae9
kuzmichit <kuzmichit@gmail.com>
2022-11-27 07:35:56 +0100

    prima di trasformazione  ub classe

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,18 +1,23 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = ( {data} ) => {
+const EmployeesList = ( { data } ) => {
 
   const elements = data.map(item => {
-    const {id, ...restItem} = item;
-		
+    const { id, ...restItem } = item;
+	
+	const newData = {};
+	newData = data.filter(item => )
     return (
-      <EmployeesListItem key={id} {...restItem} />
+      <EmployeesListItem
+        key = { id }
+        { ...restItem }
+        onDelete = { () => onDelete(id) } />
     );
   } );
-  
-  return(
-    <ul className="app-list list-group">
+
+  return (
+    <ul className = "app-list list-group">
       {elements}
     </ul>
   );
